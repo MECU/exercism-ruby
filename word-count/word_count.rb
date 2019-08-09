94942917d84c2f9467534dddc9af114cc7d69192
Michael Hoppes <mhoppes@i3logix.com>
2019-08-09 10:02:42 -0600

    refactor

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -6,9 +6,7 @@ class Phrase
   end
 
   def word_count
-    counted = {}
-    word_array.each {|word| counted[word] = counted.key?(word) ? counted[word] += 1 : 1}
-    counted
+    word_array.each_with_object(Hash.new(0)) {|word, counted| counted[word] += 1}
   end
 
   private
